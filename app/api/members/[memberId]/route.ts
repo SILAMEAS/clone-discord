35c249ebc945048a4ba8a66313430167e36f0571
SILAMEAS <sila.meas@allweb.com.kh>
2024-08-29 15:40:56 +0700

    comment-code-memberId

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -1,7 +1,3 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
-import { BadException } from "@/lib/exceptions/bad-exceptions/BadExceptions";
-import { UnauthencitationExceptions } from "@/lib/exceptions/un-authencitations/UnauthencitationExceptions";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -9,53 +5,53 @@ export async function PATCH(
   { params }: { params: { memberId: string } }
 ) {
   try {
-    const { memberId } = params;
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
-    const { role } = await req.json();
-    const [serverId] = [searchParams.get("serverId")];
-    if (!profile) {
-      return UnauthencitationExceptions();
-    }
-    if (!serverId) {
-      return BadException("Server ID missing");
-    }
-    if (!memberId) {
-      return BadException("member ID missing");
-    }
-    const server = await db.server.update({
-      where: {
-        id: serverId,
-        profileId: profile.id,
-      },
-      data: {
-        member: {
-          update: {
-            where: {
-              id: memberId,
-              profileId: {
-                not: profile.id,
-              },
-            },
-            data: {
-              role,
-            },
-          },
-        },
-      },
-      include: {
-        member: {
-          include: {
-            profile: true,
-          },
-          orderBy: {
-            role: "asc",
-          },
-        },
-      },
-    });
+    // const { memberId } = params;
+    // const profile = await currentProfile();
+    // const { searchParams } = new URL(req.url);
+    // const { role } = await req.json();
+    // const [serverId] = [searchParams.get("serverId")];
+    // if (!profile) {
+    //   return UnauthencitationExceptions();
+    // }
+    // if (!serverId) {
+    //   return BadException("Server ID missing");
+    // }
+    // if (!memberId) {
+    //   return BadException("member ID missing");
+    // }
+    // const server = await db.server.update({
+    //   where: {
+    //     id: serverId,
+    //     profileId: profile.id,
+    //   },
+    //   data: {
+    //     member: {
+    //       update: {
+    //         where: {
+    //           id: memberId,
+    //           profileId: {
+    //             not: profile.id,
+    //           },
+    //         },
+    //         data: {
+    //           role,
+    //         },
+    //       },
+    //     },
+    //   },
+    //   include: {
+    //     member: {
+    //       include: {
+    //         profile: true,
+    //       },
+    //       orderBy: {
+    //         role: "asc",
+    //       },
+    //     },
+    //   },
+    // });
 
-    return NextResponse.json(server);
+    return NextResponse.json(null);
   } catch (error) {
     return new NextResponse("Internel Error", { status: 500 });
   }
